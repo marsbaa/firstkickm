a269d290af108bab7579b1a3189a60946e692633
marsbaa <rayyee@Rays-MacBook-Pro.local>
2016-11-04 23:59:21 +1100

    Nav Bar

--- server.js
+++ server.js
@@ -25,39 +25,6 @@ app.use(express.static(__dirname + '/public'));
 // a script tag to your application's JavaScript file(s).
 
 
-var smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-      user: process.env.GMAIL_U, // Your email id
-      pass: process.env.GMAIL_P // Your password
-    }
-};
-var smtpTransport = nodemailer.createTransport(smtpConfig);
-
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
-
-
-app.get('/send',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-        subject : req.query.subject,
-        text : req.query.text,
-        html : req.query.html
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-});
-});
 
 app.listen(PORT);
 console.log("server started on port " + PORT);
