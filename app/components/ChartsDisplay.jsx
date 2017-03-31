7623ef2a2429c06e03b3c461ebca298eed4b02af
marsbaa <ray@marsbaa.com>
2017-03-31 16:32:10 +1100

    Charts test

--- app/components/ChartsDisplay.jsx
+++ app/components/ChartsDisplay.jsx
@@ -4,7 +4,7 @@ import {Row, Col} from 'react-bootstrap'
 import {connect} from 'react-redux';
 import {btn} from 'styles.css'
 var actions = require('actions');
-import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'Recharts'
+import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 const data = [
       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
       {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
